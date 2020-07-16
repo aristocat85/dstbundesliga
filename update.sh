3e8aab6fa47191273ce74739f6293c2d40b02be5
Daniel Engelhardt <daniel85engelhardt@gmail.com>
2020-07-16 09:56:08 +0200

    Updated Install-Script

--- update.sh
+++ update.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 supervisorctl stop uwsgi
 git pull
+pip3.8 install -r requirements.txt --user
 python3.8 manage.py migrate
 python3.8 manage.py collectstatic
 supervisorctl start uwsgi
