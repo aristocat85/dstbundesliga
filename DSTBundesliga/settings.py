042e968271037bbb0b9d851bdfeb6f77ba8d632e
Daniel Engelhardt <daniel85engelhardt@gmail.com>
2020-08-02 13:24:58 +0200

    Fixed Admin Styles

--- DSTBundesliga/settings.py
+++ DSTBundesliga/settings.py
@@ -35,6 +35,7 @@ SITE_ID = 1
 # Application definition
 
 INSTALLED_APPS = [
+    'djangocms_admin_style',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
