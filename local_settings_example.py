185784b1a7f2c2caf410e554a31ee61969257fca
Daniel Engelhardt <daniel85engelhardt@gmail.com>
2021-05-02 18:34:02 +0200

    Added python-dotenv as requirement

--- local_settings_example.py
+++ local_settings_example.py
@@ -5,7 +5,7 @@ DATABASES = {
     }
 }
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 X_FRAME_OPTIONS = "ALLOWALL"
 XS_SHARING_ALLOWED_METHODS = ["POST", "GET", "OPTIONS", "PUT", "DELETE"]
