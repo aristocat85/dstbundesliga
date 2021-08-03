8fe4acd6911350fe0111fc973ffe9b436ca54e65
Daniel Engelhardt <daniel.engelhardt@codecentric.de>
2021-08-03 18:23:11 +0200

    Added Commishguide

--- DSTBundesliga/views.py
+++ DSTBundesliga/views.py
@@ -1,21 +1,5 @@
-from django.contrib.auth import authenticate, login
 from django.shortcuts import redirect
-from django.shortcuts import render
-
-'''
-def login(request):
-    return render(request, 'login.html')
 
 
-def authenticate(request):
-    username = request.POST['username']
-    password = request.POST['password']
-    user = authenticate(request, username=username, password=password)
-    if user is not None:
-        login(request, user)
-        response = redirect('/')
-    else:
-        response = redirect('/login')
-    return response
-
-'''
+def commishguide(request):
+    return redirect("/shared/files/1627921237/17/")
