2b77bdc60788daaffc9c2e84e28825f8869aa379
Daniel Engelhardt <daniel.engelhardt@codecentric.de>
2021-08-27 18:46:14 +0200

    Fix for Draft-Stats

--- DSTBundesliga/apps/leagues/tables.py
+++ DSTBundesliga/apps/leagues/tables.py
@@ -68,7 +68,7 @@ class DraftsADPTable(tables.Table):
 
     ranking = tables.Column(verbose_name='Platz', empty_values=(), orderable=False, attrs={"td": {"class": "ranking"}, "th": {"class": "ranking"}}, )
     player = tables.TemplateColumn(verbose_name='Spieler', template_name="Columns/player.html", empty_values=(), attrs={"td": {"class": "player"}, "th": {"class": "player"}})
-    pos = tables.Column(verbose_name='Position', accessor="position", attrs={"td": {"class": "player_position"}, "th": {"class": "position"}})
+    pos = tables.Column(verbose_name='Position', accessor="player_position", attrs={"td": {"class": "player_position"}, "th": {"class": "position"}})
     adp = tables.Column(verbose_name='ADP', attrs={"td": {"class": "adp"}, "th": {"class": "adp"}})
     highest_pick = tables.Column(verbose_name='Highest', attrs={"td": {"class": "highest_pick"}, "th": {"class": "highest_pick"}})
     lowest_pick = tables.Column(verbose_name='Lowest', attrs={"td": {"class": "lowest_pick"}, "th": {"class": "lowest_pick"}})
