4d48738f8f8fa6ccd0fad567bcdb99d78e729c2b
Daniel Engelhardt <daniel.engelhardt@codecentric.de>
2022-07-30 19:50:13 +0200

    * Bugfix

--- DSTBundesliga/apps/services/season_service.py
+++ DSTBundesliga/apps/services/season_service.py
@@ -6,9 +6,8 @@ from DSTBundesliga.apps.leagues.models import PlayoffMatchup, Season, League, Fi
 
 
 def is_registration_open():
-    print(datetime.date.fromisoformat(settings.REGISTRATION_STARTS))
     return datetime.date.fromisoformat(settings.REGISTRATION_STARTS) \
-           < datetime.date.today() \
+           <= datetime.date.today() \
            <= datetime.date.fromisoformat(settings.REGISTRATION_STOPS)
 
 
