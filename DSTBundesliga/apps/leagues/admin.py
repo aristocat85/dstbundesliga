f95acb525a1c9c7164b26765cb2bfa6618c7329c
Daniel Engelhardt <daniel.engelhardt@codecentric.de>
2022-08-09 18:50:11 +0200

    * Small improvements

--- DSTBundesliga/apps/leagues/admin.py
+++ DSTBundesliga/apps/leagues/admin.py
@@ -12,9 +12,9 @@ class FinalSeasonStandingAdmin(admin.ModelAdmin):
         if obj.roster.owner:
             return str(obj.roster.owner)
         return "deleted"
-
+    search_fields = ['roster__owner__display_name', 'roster__owner__sleeper_id', 'league__sleeper_name']
     list_display = ['points_ranking_overall', 'points_ranking_on_level', 'points_ranking_in_league', 'get_owner', 'league', 'season', 'rank_in_league', 'points', 'points_decimal']
-    list_filter = ('season', 'league__level', 'points_ranking_on_level', 'points_ranking_in_league')
+    list_filter = ('season', 'league__level', 'rank_in_league')
 
 
 admin.site.register(Season, SeasonAdmin)
