468f2067acf9ca59a4b77d0485d4ffcf4ef0f201
Daniel Engelhardt <daniel.engelhardt@codecentric.de>
2024-08-12 10:07:41 +0200

    DSV Download for Season Registrations

--- DSTBundesliga/apps/dstffbl/admin.py
+++ DSTBundesliga/apps/dstffbl/admin.py
@@ -70,6 +70,61 @@ def download_season_users_csv(modeladmin, request, queryset):
     return response
 
 
+def download_season_registrations_csv(modeladmin, request, queryset):
+    REGION = {
+        1: "Nord",
+        2: "Ost",
+        3: "Süd",
+        4: "West",
+        5: "Ausland",
+    }
+
+    response = HttpResponse(content_type="text/csv")
+    response["Content-Disposition"] = "attachment;filename=season_registrations.csv"
+    writer = csv.writer(response)
+
+    writer.writerow(
+        [
+            "Email",
+            "Sleeper Username",
+            "Sleeper ID",
+            "Region",
+            "Neuer Spieler",
+            "Liga letzte Saison",
+            "Liga letzte Saison ID",
+            "Potentieller Komissioner",
+            "Registrierzeitpunkt",
+        ]
+    )
+
+    for su in queryset:
+        new_player = "Ja" if su.new_player else "Nein"
+        league, league_id = (
+            (su.last_years_league.sleeper_name, su.last_years_league.sleeper_id)
+            if su.last_years_league
+            else ("", "")
+        )
+        commish = "Ja" if su.possible_commish else "Nein"
+        region = REGION.get(su.region)
+        writer.writerow(
+            [
+                su.user.email,
+                su.dst_player.display_name,
+                su.sleeper_id,
+                region,
+                new_player,
+                league,
+                league_id,
+                commish,
+                su.registration_ts.astimezone(
+                    pytz.timezone("Europe/Berlin")
+                ).strftime("%d.%m.%Y, %H:%M:%S"),
+            ]
+        )
+
+    return response
+
+
 class NewsAdmin(admin.ModelAdmin):
     list_display = ["date", "title", "content"]
     ordering = ["-date"]
